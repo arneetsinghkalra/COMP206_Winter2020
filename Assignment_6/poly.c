296020be540099f1807de917023e2ae6dc6ca098
arneetsinghkalra <a.s.kalra1999@gmail.com>
2020-04-09 18:08:45 -0400

    Instructions updated

---
+++ Assignment_6/poly.c
@@ -0,0 +1,23 @@
+/*
+ * Program to implement a simple banking application. There are 3 capabilities.
+ * You can add an account number, make a deposit, and make a withdrawal.
+ * Data is stored in a CSV file: bankdata.csv 
+ ************************************************************************
+ * Author       Dept.              Date              History
+ ************************************************************************
+ * Arneet K     Software Eng      April 9, 2020      Initial Version
+*/ 
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    //Verify argument is provided
+    if (argc < 1) {
+        fprintf(stderr,"Error: Input File Name\n");
+        exit(-1);
+    }
+
+    
+}
+
